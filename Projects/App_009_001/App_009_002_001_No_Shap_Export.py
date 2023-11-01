6f952f5d0543ae56edd6271b470db3d54c643719
Luismbpr <97373331+Luismbpr@users.noreply.github.com>
2023-11-01 15:35:00 -0600

    Update App_009_002_001_No_Shap_Export.py

--- Projects/App_009_001/App_009_002_001_No_Shap_Export.py
+++ Projects/App_009_001/App_009_002_001_No_Shap_Export.py
@@ -195,5 +195,5 @@ st.write("""
          """)
 st.write('')
 st.write('')
-st.write('###### *Code based on [Free Code Camp](https://www.freecodecamp.org/). Special Thanks to Free Code Camp and instructor Chanin Nantasenamat*')
-st.write("##### Thank you kindly to all who make information and knowledge available for free.")
+st.write('###### [Special Thanks to Free Code Camp and Chanin Nantasenamat](https://www.freecodecamp.org/).')
+st.write('###### Thank you kindly to all who make information and knowledge available for free.')
